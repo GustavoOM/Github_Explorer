fe0a25f670f3e6d0de73a75407a0a6a8a1e4d278
GustavoOM <gumartins2001@gmail.com>
2020-07-06 07:10:17 -0300

    :sparkles: challenge completed

---
+++ build/precache-manifest.7972150809531359629ec25a254e6fce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01ba4063d8428425e4f5cf768161d496",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00ec8ad62efe20c5229d",
+    "url": "/static/js/2.7cdce96a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7cdce96a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "737d3a5f085010056a3f",
+    "url": "/static/js/main.e2bd3526.chunk.js"
+  },
+  {
+    "revision": "88dfb91094e5338d4466",
+    "url": "/static/js/runtime-main.868c6d53.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/static/media/github-backgroud.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/static/media/logo.04f6798f.svg"
+  }
+]);
